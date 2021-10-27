aa9c02726a12c78a4f02e3b24cea74d07d791b85
dhonstudio <doon13@gmail.com>
2021-10-27 10:56:16 +0700

    remove Problems

--- lib/sliverappbar.dart
+++ lib/sliverappbar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
 
